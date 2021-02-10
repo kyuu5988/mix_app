bc96685aa5c2c76eaf7f0d52434976758cf6fa4f
kyuu5988 <kyuu5988@yahoo.co.jp>
2021-02-10 16:28:18 +0900

    詳細ページ

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,11 +1,10 @@
 class TweetsController < ApplicationController
-
-
+  before_action :set_tweet, only: [:edit, :show]#6
 
 
   def index#1
-    #@tweets = Tweet.all#1
-    @tweets = Tweet.all.order("created_at DESC")
+    #@tweets = Tweet.all#1-1
+    @tweets = Tweet.all.order("created_at DESC")#1-2
   end
 
   def new#2
@@ -13,9 +12,14 @@ class TweetsController < ApplicationController
   end
 
   def create#3    
-    Tweet.create(tweet_params)
-
-    redirect_to root_path
+    #Tweet.create(tweet_params)#3-1
+    @tweet = Tweet.new(tweet_params)#3-2
+    if @tweet.save
+      redirect_to root_path
+    else
+      flash[:not_input] = "何も入力されていません。"
+      render :new
+    end
   end
 
   def destroy#5
@@ -23,7 +27,13 @@ class TweetsController < ApplicationController
     #binding.pry #検証用
     tweet.destroy
 
-    redirect_to root_path
+    redirect_to root_path, notice: '投稿が削除されました。'
+  end
+
+  def edit#6  
+  end
+
+  def show#6    
   end
 
 
@@ -32,6 +42,10 @@ class TweetsController < ApplicationController
   def tweet_params#4
     params.require(:tweet).permit(:text, :image)#.merge(user_id: current_user.id)
     # :imageはactive storage用
+  end
+
+  def set_tweet#6
+    @tweet = Tweet.find(params[:id])
   end
 
 
